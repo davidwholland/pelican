cc3f11b16db9d460b9265d7b5dc54bbea174652a
David Holland <dave.holl@gmail.com>
2014-01-21 14:14:34 -0800

    added autofac registration to global.asax

--- pelican/Global.asax.cs
+++ pelican/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using Pelican.App_Start;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -9,6 +10,8 @@ namespace Pelican
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            DependenciesConfig.RegisterDependencies();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
