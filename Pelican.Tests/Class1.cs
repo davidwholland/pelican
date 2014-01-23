79e1994cc9c7bc3b17f312e4c2e187233b4325c3
David Holland <dave.holl@gmail.com>
2014-01-23 11:15:24 -0800

    added xunit test framework, modified msbuild script to run tests

---
+++ Pelican.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pelican.Tests
+{
+    public class Initial
+    {
+        [Fact]
+        public void MathTest()
+        {
+            Assert.Equal(5, 2 + 3);
+        }
+    }
+}
