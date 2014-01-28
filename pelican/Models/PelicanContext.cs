8f7041ffb7424818cc21efc37ad3d593fa0083d0
David Holland <dave.holl@gmail.com>
2014-01-27 17:20:21 -0800

    added DbContext class

---
+++ pelican/Models/PelicanContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Pelican.Models
+{
+    public class PelicanContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+
+        public PelicanContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
