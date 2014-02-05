b4fa87729b5ff998553e9360462091f79a9fd1e1
David Holland <dave.holl@gmail.com>
2014-02-04 20:57:41 -0800

    added tests, removed unneeded file

---
+++ Pelican.Tests/GivenACultureHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Pelican.Helpers;
+
+namespace Pelican.Tests
+{
+    public class GivenACultureHelper
+    {
+        [Fact]
+        public void WhenGetImplementedCultureIsCalledFor_en()
+        {
+            var implementedCulture = CultureHelper.GetImplementedCulture("en");
+            Assert.Equal("en-US", implementedCulture);
+        }
+
+        [Fact]
+        public void WhenGetImplementedCultureIsCalledFor_fr()
+        {
+            var implementedCulture = CultureHelper.GetImplementedCulture("fr");
+            Assert.Equal("en-US", implementedCulture);
+        }
+
+        [Fact]
+        public void WhenGetImplementedCultureIsCalledWithEmptyString()
+        {
+            var implementedCulture = CultureHelper.GetImplementedCulture("");
+            Assert.Equal("en-US", implementedCulture);
+        }
+    }
+}
